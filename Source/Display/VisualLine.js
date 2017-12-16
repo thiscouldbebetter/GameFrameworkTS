282302f39d6ee7915c56c08bcb8b352c4698d873
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2017-12-16 09:28:14 -0500

    Added VisualLine.

---
+++ Source/Display/VisualLine.js
@@ -0,0 +1,40 @@
+
+function VisualLine(fromPos, toPos, color)
+{
+	this.fromPos = fromPos;
+	this.toPos = toPos;
+	this.color = color;
+
+	// Helper variables.
+
+	this.drawPosFrom = new Coords();
+	this.drawPosTo = new Coords();
+}
+
+{
+	VisualLine.prototype.draw = function(universe, display, drawable, loc)
+	{
+		var drawPosFrom = this.drawPosFrom.overwriteWith
+		(
+			loc.pos
+		).add
+		(
+			this.fromPos
+		);
+
+		var drawPosTo = this.drawPosTo.overwriteWith
+		(
+			loc.pos
+		).add
+		(
+			this.toPos
+		);
+
+		display.drawLine
+		(
+			drawPosFrom,
+			drawPosTo,
+			this.color
+		);
+	}
+}
