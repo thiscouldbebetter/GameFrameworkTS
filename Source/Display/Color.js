1263ca1736afba02dfe0d2a0170b0972470ffcc7
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2018-02-23 12:15:21 -0500

    Added more default colors to Color.Instances.

--- Source/Display/Color.js
+++ Source/Display/Color.js
@@ -3,7 +3,7 @@ function Color(name, code, componentsRGBA)
 {
 	this.name = name;
 	this.code = code;
-	this.componentsRGBA = componentsRGBA; 
+	this.componentsRGBA = componentsRGBA;
 }
 
 {
@@ -26,33 +26,39 @@ function Color(name, code, componentsRGBA)
 
 		this.Black = new Color("Black", "k", [0, 0, 0, 1] );
 		this.Blue = new Color("Blue", "b", [0, 0, 1, 1] );
+		this.BlueDark = new Color("BlueDark", "B", [0, 0, .5, 1] );
 		this.Brown = new Color("Brown", "o", [0.5, 0.25, 0, 1] );
 		this.Cyan = new Color("Cyan", "c", [0, 1, 1, 1] );
 		this.Gray = new Color("Gray", "a", [0.5, 0.5, 0.5, 1] );
 		this.GrayDark = new Color("GrayDark", "A", [0.25, 0.25, 0.25, 1] );
 		this.GrayLight = new Color("GrayLight","@", [0.75, 0.75, 0.75, 1] );
 		this.Green = new Color("Green",	"g", [0, 1, 0, 1] );
+		this.GreenDark = new Color("GreenDark", "G", [0, .5, 0, 1] );
 		this.Orange = new Color("Orange", "o", [1, 0.5, 0, 1] );
 		this.Red = new Color("Red", "r", [1, 0, 0, 1] );
+		this.RedDark = new Color("RedDark", "R", [.5, 0, 0, 1] );
 		this.Violet = new Color("Violet", "v", [1, 0, 1, 1] );
 		this.White = new Color("White", "w", [1, 1, 1, 1] );
 		this.Yellow = new Color("Yellow", "y", [1, 1, 0, 1] );
 		this.YellowDark = new Color("Yellow", "Y", [.5, .5, 0, 1] );
 
-		this._All = 
+		this._All =
 		[
 			this._Transparent,
 
 			this.Black,
 			this.Blue,
+			this.BlueDark,
 			this.Brown,
 			this.Cyan,
 			this.Gray,
 			this.GrayDark,
 			this.GrayLight,
 			this.Green,
+			this.GreenDark,
 			this.Orange,
 			this.Red,
+			this.RedDark,
 			this.Violet,
 			this.White,
 			this.Yellow,
@@ -88,11 +94,11 @@ function Color(name, code, componentsRGBA)
 	{
 		if (this._systemColor == null)
 		{
-			this._systemColor = 
-				"rgba(" 
-				+ Math.floor(255 * this.componentsRGBA[0]) + ", " 
-				+ Math.floor(255 * this.componentsRGBA[1]) + ", " 
-				+ Math.floor(255 * this.componentsRGBA[2]) + ", " 
+			this._systemColor =
+				"rgba("
+				+ Math.floor(255 * this.componentsRGBA[0]) + ", "
+				+ Math.floor(255 * this.componentsRGBA[1]) + ", "
+				+ Math.floor(255 * this.componentsRGBA[2]) + ", "
 				+ this.componentsRGBA[3]
 				+ ")";
 		}
