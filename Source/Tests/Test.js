a3d4193772b0d362d740c40c05c24bf9b68e6081
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2018-02-23 17:01:22 -0500

    Re-added Test.js, which was removed to get around problems changing filename case.

---
+++ Source/Tests/Test.js
@@ -0,0 +1,56 @@
+
+function Test()
+{
+	// Static class.
+}
+{
+	Test.assertExpectedEqualToActual = function(expected, actual, message)
+	{
+		if (expected != actual)
+		{
+			if (message == null)
+			{
+				message = "Expected: '" + expected + "', Actual: '" + actual + "'.";
+			}
+
+			console.log("Error running test: " + message);
+			console.trace();
+
+			throw message;
+		}
+	}
+
+	Test.runTestsInFixture = function(testFixture)
+	{
+		var testFixtureName = testFixture.constructor.name;
+
+		for (var testName in testFixture)
+		{
+			var testToRun = testFixture[testName];
+			if (testToRun.constructor.name == "Function")
+			{
+				try
+				{
+					testToRun.call(testFixture);
+				}
+				catch (ex)
+				{
+					console.log("Test failed: " + testFixtureName + "." + testName + ".");
+					throw ex;
+				}
+			}
+		}
+
+		console.log("All tests in test fixture " + testFixtureName + " passed.");
+	}
+
+	Test.runTestsInFixtures = function(testFixtures)
+	{
+		for (var i = 0; i < testFixtures.length; i++)
+		{
+			var testFixture = testFixtures[i];
+			Test.runTestsInFixture(testFixture);
+		}
+	}
+
+}
