09eb07c681660f7b4df6e4ad7d23a72a884c1905
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2018-08-21 18:54:17 -0400

    Created VisualAnchor to pin child visuals to a specific position.

---
+++ Source/Display/VisualAnchor.js
@@ -0,0 +1,20 @@
+
+function VisualAnchor(child, posToAnchorAt)
+{
+	this.child = child;
+	this.posToAnchorAt = posToAnchorAt;
+
+	// Helper variables.
+	this.posSaved = new Coords();
+}
+
+{
+	VisualAnchor.prototype.draw = function(universe, world, display, drawable)
+	{
+		var drawablePos = drawable.loc.pos;
+		this.posSaved.overwriteWith(drawablePos);
+		drawablePos.overwriteWith(this.posToAnchorAt);
+		this.child.draw(universe, world, display, drawable);
+		drawablePos.overwriteWith(this.posSaved);
+	}
+}
