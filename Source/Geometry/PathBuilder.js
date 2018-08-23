e3740c6266ac0a21eddb6f2e253e1a5d1f0b4569
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2018-08-22 22:17:51 -0400

    Added PathBuilder class.

---
+++ Source/Geometry/PathBuilder.js
@@ -0,0 +1,25 @@
+
+function PathBuilder()
+{
+	// Do nothing.
+}
+{
+	PathBuilder.prototype.star = function(numberOfPoints, ratioOfInnerRadiusToOuter)
+	{
+		var numberOfVertices = numberOfPoints * 2;
+		var turnsPerVertex = 1 / numberOfVertices;
+		var polar = new Polar(0, 1);
+
+		var vertices = [];		
+		for (var i = 0; i < numberOfVertices; i++)
+		{
+			polar.radius = (i % 2 == 0 ? 1 : ratioOfInnerRadiusToOuter);
+			var vertex = polar.toCoords( new Coords() );
+			vertices.push(vertex);
+			polar.azimuthInTurns += turnsPerVertex;
+		}
+		
+		var returnValue = new Path(vertices);
+		return returnValue;
+	}
+}
