f3e281689dbea8c4866cbadda0898f3976795965
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2019-11-30 22:33:33 -0500

    Added Drawable.isVisible.

--- Source/Model/Drawable.js
+++ Source/Model/Drawable.js
@@ -1,12 +1,23 @@
 
-function Drawable(visual)
+function Drawable(visual, isVisible)
 {
 	this.visual = visual;
+	this.isVisible = (isVisible == null ? true : isVisible);
 }
 {
 	Drawable.prototype.updateForTimerTick = function(universe, world, place, entity)
 	{
-		this.loc = entity.Locatable.loc;
-		this.visual.draw(universe, world, universe.display, this, entity);
+		if (this.isVisible)
+		{
+			this.loc = entity.Locatable.loc;
+			this.visual.draw(universe, world, universe.display, this, entity);
+		}
 	};
+
+	// cloneable
+
+	Drawable.prototype.clone = function()
+	{
+		return new Drawable(this.visual, this.isVisible);
+	}
 }
