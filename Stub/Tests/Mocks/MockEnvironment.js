94da021b26db2464a3ea1b7d7d174fc1782cc44f
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2021-06-26 20:44:55 -0400

    Moved Stub game directory one level up.

---
+++ Stub/Tests/Mocks/MockEnvironment.js
@@ -0,0 +1,17 @@
+"use strict";
+class MockEnvironment {
+    constructor() {
+        this.universe = this.universeCreate();
+    }
+    universeCreate() {
+        var universe = Universe.default();
+        universe.world = universe.worldCreate();
+        universe.world.defn = new WorldDefn([
+            [PlaceDefn.default()]
+        ]);
+        universe.initialize(() => { });
+        universe.profile = Profile.anonymous();
+        universe.world.initialize(universe);
+        return universe;
+    }
+}
