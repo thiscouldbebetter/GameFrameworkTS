c3863ca5076a85033221960ea9fc237e4b9ef367
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-07-19 11:41:17 -0400

    Removed obsolete BoxRotated and ShapeHelper from Game.html.  Added Game-RunInChrome.bat to support development from Stub directory.

---
+++ Source/_Scripts/Game-RunInChrome.bat
@@ -0,0 +1,11 @@
+@echo off
+echo This script starts the Chrome (or Chromium) browser
+echo with the --allow-file-access-from-files switch,
+echo which is necessary to access content files
+echo when running from file, rather than a web server. 
+echo This won't work if:
+echo 1. Chrome is not installed,
+echo 2. Chrome's path is not part of the "PATH" environment variable, or
+echo 3. any Chrome window is already running without the "--allow-file-access-from-files" switch.
+
+chrome --allow-file-access-from-files %cd%/../Game.html
