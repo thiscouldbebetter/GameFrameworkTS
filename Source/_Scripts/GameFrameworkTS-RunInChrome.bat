5f1a4ce5c940e02b98dc091bb0855c050b40296f
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-08-10 12:15:36 -0400

    Adjusted run scripts.

--- Source/_Scripts/GameFrameworkTS-RunInChrome.bat
+++ Source/_Scripts/GameFrameworkTS-RunInChrome.bat
@@ -1,6 +1,7 @@
 @echo off
-echo This script starts the Chrome (or Chromium) browser
-echo with the --allow-file-access-from-files switch,
+echo This script attempts to open GameFrameworkTS.html
+echo within a Chrome (or Chromium) browser
+echo running the --allow-file-access-from-files switch,
 echo which is necessary to access content files
 echo when running from file, rather than a web server. 
 echo This won't work if:
@@ -8,4 +9,5 @@ echo 1. Chrome is not installed,
 echo 2. Chrome's path is not part of the "PATH" environment variable, or
 echo 3. any Chrome window is already running without the "--allow-file-access-from-files" switch.
 
+cd ..
 chrome --allow-file-access-from-files %cd%/GameFrameworkTS.html
--- Source/_Scripts/_RunChromeWithFileAccess.bat
+++ Source/_Scripts/_RunChromeWithFileAccess.bat
@@ -1,6 +1,9 @@
-rem This won't work if:
-rem 1. Chrome is not installed,
-rem 2. Chrome's path is not part of the "PATH" environment variable, or
-rem 3. any Chrome window is already running without the "--allow-file-access-from-files" switch.
+@echo off
+
+echo Attempting to start the Chrome (or Chromium) browser with the --allow-file-access-from-files switch.
+echo This won't work if:
+echo 1. Chrome is not installed,
+echo 2. Chrome's path is not part of the "PATH" environment variable, or
+echo 3. any Chrome window is already running without the "--allow-file-access-from-files" switch.
 
 chrome --allow-file-access-from-files
